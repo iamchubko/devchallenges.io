7edd9abd7ea6891e3ba2ad5625cf95993d154062
iamchubko <56153711+iamchubko@users.noreply.github.com>
2020-12-20 00:51:43 +0300

    Rewrote function and some more tweaks
    * Renamed some variables with more describing names; remove excess variables;
    * Rewrote and merged increment/decrement functions into one;
    * Merged event listeners into one;

--- public/checkout-page/script.js
+++ public/checkout-page/script.js
@@ -1,53 +1,53 @@
-const plusButtons = document.querySelectorAll('.plus');
-const minusButtons = document.querySelectorAll('.minus');
+const customQtyInputs = document.querySelectorAll('.number-input');
+const qtyInputs = document.querySelectorAll('.item__quantity');
 
-const inputsQuantity = document.querySelectorAll('.item__quantity');
-const itemsPrice = document.querySelectorAll('.sale');
-
-let priceArray = [];
-let quantityArray = [];
-
-let priceSum;
-
-let priceShipping = document.getElementById('price-shipping');
-priceShipping = Number(priceShipping.textContent.replace('$', ''));
-
-let priceTotal = document.getElementById('price-total');
+    // saveQty() and calcTotal() variables
+    let qtyInputsValue = [];
+    const itemsPrice = document.querySelectorAll('.sale');
+    const priceTotal = document.getElementById('price-total');
+    let priceShipping = document.getElementById('price-shipping');
+    priceShipping = Number(priceShipping.textContent.replace('$', ''));
+    let itemsPriceArray = [];
+    let priceSum;
 
 
 
 
-function increment() {
-    this.parentNode.querySelector('input[type=number]').stepUp();
-    calcTotal();
-    saveQty();
-
-    if (this.parentNode.querySelector('input[type=number]').value > '0') {
-        this.parentNode.querySelector('.minus').disabled = false;
-        this.parentNode.querySelector('.minus').style.cssText = 'cursor: pointer';
+function changeQty(e) { // changing qty in input fields
+    if (e.target.className === 'plus') {
+        e.currentTarget.querySelector('input[type=number').stepUp();
+    } else if (e.target.className === 'minus') {
+        e.currentTarget.querySelector('input[type=number').stepDown();
     }
-}
-
-function decrement() {
-    this.parentNode.querySelector('input[type=number]').stepDown();
-    calcTotal();
-    saveQty();
+   
+    if (e.currentTarget.querySelector('input[type=number]').value === '0') {
+        e.currentTarget.querySelector('.minus').disabled = true;
+        e.currentTarget.querySelector('.minus').style.cssText = 'cursor: initial';
+    } else if (e.currentTarget.querySelector('input[type=number]').value > '0') {
+        e.currentTarget.querySelector('.minus').disabled = false;
+        e.currentTarget.querySelector('.minus').style.cssText = 'cursor: pointer';
+    }
     
-    if (this.parentNode.querySelector('input[type=number]').value === '0') {
-        this.disabled = true;
-        this.style.cssText = 'cursor: initial';
-    }
+    saveQty();
+    calcTotal();
 }
 
-
+function saveQty() { // saving qty of items in localStorage
+    for (let i = 0; i < qtyInputs.length; i++) {
+        qtyInputsValue[i] = qtyInputs[i].value;
+        localStorage.setItem('qty', qtyInputsValue);
+    }
+}
 
 function calcTotal() { // calculates total price of items
-    priceSum = 0;
+    priceSum = 0; // resets value on each call
 
-    for (let i = 0; i < inputsQuantity.length; i++) {
-        priceArray[i] = Number(itemsPrice[i].textContent.replace('$', ''));
-        quantityArray[i] = Number(inputsQuantity[i].value);
-        priceSum += priceArray[i] * quantityArray[i];
+    for (let i = 0; i < qtyInputs.length; i++) {
+        if (qtyInputs[i].value >= '0') {
+            itemsPriceArray[i] = Number(itemsPrice[i].textContent.replace('$', ''));
+            qtyInputsValue[i] = Number(qtyInputs[i].value);
+            priceSum += itemsPriceArray[i] * qtyInputsValue[i];
+        }
     }
     
     priceSum = (priceSum + priceShipping).toFixed(2);
@@ -55,43 +55,10 @@ function calcTotal() { // calculates total price of items
 }
 
 
-
-plusButtons.forEach(e => {
-    e.addEventListener('click', increment);
-});    
-
-minusButtons.forEach(e => {
-    e.addEventListener('click', decrement);
-});    
-
-
-
-// for keyboard arrows
-inputsQuantity.forEach(e => {
-    e.addEventListener('input', calcTotal);
-});
-
-
-
-
-
-
-
-// ---------saving quantity of items---------
-
-let numberQuantity = [];
-
-inputsQuantity.forEach(e => {
-    e.addEventListener('input', saveQty);
-});
-    
-
-function saveQty() {
-    for (let i = 0; i < inputsQuantity.length; i++) {
-        numberQuantity[i] = inputsQuantity[i].value;
-        localStorage.setItem('qty', numberQuantity);
-    }
-}
+    customQtyInputs.forEach(e => {
+        e.addEventListener('click', changeQty);
+        e.addEventListener('input', changeQty);
+    });   
 
 
 
@@ -100,15 +67,15 @@ function saveQty() {
 
 // -------saving information when checkbox checked----------
 
-const inputsForSave = document.querySelectorAll('fieldset input, fieldset select');
+const userDataInputs = document.querySelectorAll('fieldset input, fieldset select');
 const saveCheck = document.getElementById('save');
 
-let forSaveArray = Array.from(inputsForSave);
+let forSaveArray = Array.from(userDataInputs);
 let savedData = [];
 
 saveCheck.addEventListener('change', saveInputsFunc);
 
-inputsForSave.forEach(e => {
+userDataInputs.forEach(e => {
     e.addEventListener('input', saveInputsFunc);
 });
 
@@ -132,34 +99,33 @@ function saveInputsFunc() {
 
 
 window.onload = function() {
-    // item qty
-    if (localStorage.getItem('qty') !== null) {
-        numberQuantity = localStorage.getItem('qty').split(',');
-        for (let i = 0; i < inputsQuantity.length; i++) {
-            inputsQuantity[i].value = numberQuantity[i];
+    // restores item qty
+    if (localStorage.getItem('qty') !== null) { // when item exists
+        qtyInputsValue = localStorage.getItem('qty').split(',');
+        for (let i = 0; i < qtyInputs.length; i++) {
+            qtyInputs[i].value = qtyInputsValue[i];
         }
     }
 
     // disables button if qty === 0
-    for (let i = 0; i < inputsQuantity.length; i++) {
-        if (inputsQuantity[i].value === '0') {
-            inputsQuantity[i].parentNode.querySelector('.minus').disabled = true;
-            inputsQuantity[i].parentNode.querySelector('.minus').style.cssText = 'cursor: initial';
-
+    for (let i = 0; i < qtyInputs.length; i++) {
+        if (qtyInputs[i].value === '0') {
+            customQtyInputs[i].querySelector('.minus').disabled = true;
+            customQtyInputs[i].querySelector('.minus').style.cssText = 'cursor: initial';
         }
     }
     
     calcTotal();
 
     
-    // text inputs
+    // restores text inputs
     saveCheck.checked = localStorage.getItem('saveCheck');
     
-    if (saveCheck.checked === true) {
-        for (let i = 0; i < inputsForSave.length; i++) {
+    if (saveCheck.checked === true) { // don't run when localStorage is removed
+        for (let i = 0; i < userDataInputs.length; i++) {
             savedData = localStorage.getItem('data').split(',');
             
-            inputsForSave[i].value = savedData[i];
+            userDataInputs[i].value = savedData[i];
         }
     }
 };
